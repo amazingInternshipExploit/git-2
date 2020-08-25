308f4a351387d6a7ba8605355cd93c1aea2a8b12
Intership Student <shinobi_2@shinobi_2.intership.yonder.ninja>
2020-08-25 19:10:59 +0200

    second commit

---
+++ curl2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+curl http://find-the-orphan.git.challenges/css/loader.css >> css_loader1.css
+     curl http://find-the-orphan.git.challenges/css/normaliz.css >> css_normalize1.css
+     curl http://find-the-orphan.git.challenges/css/normalize.css >> css_normalize1.css
+     curl http://find-the-orphan.git.challenges/css/font-awesome.min.css >> css_normalize1.css
+     curl http://find-the-orphan.git.challenges/css/font-awesome.min.css >> css_fa1.css
+     curl http://find-the-orphan.git.challenges/css/normalize.css > css_normalize1.css
+     curl http://find-the-orphan.git.challenges/css/font-awesome.min.css > css_fa1.css
+     curl http://find-the-orphan.git.challenges/css/style.css > css_style1.css
+     curl http://find-the-orphan.git.challenges/js/jquerry.js > jquery1.js
+     curl http://find-the-orphan.git.challenges/js/plugins.js > plugins1.js
+     curl http://find-the-orphan.git.challenges/js/jquery.countdown.min.js > countdown1.js
+     curl http://find-the-orphan.git.challenges/js/main.js > main1.js
